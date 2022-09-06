bc822c85de1a3c3e14bc5f9902471ab93dde772c
Sokaster <danenkov.dmitriy@list.ru>
2022-09-06 21:57:11 +0300

    Add project files.

---
+++ DogMVCProject/Controllers/PetController.cs
@@ -0,0 +1,28 @@
+﻿using DogMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogMVCProject.Controllers
+{
+    public class PetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult VetApplication()
+        {
+            ModelState.Clear();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult VetApplication(DogViewModel dog)
+        {
+            System.Console.WriteLine($" Dog: {dog.PetName} Mass: {dog.MassOfDog} Age: {dog.AgeOfDog} Dog Owner: {dog.Name} was saved in database");
+            
+            //ModelState.Clear();
+            return VetApplication();
+        }
+    }
+}
---
+++ DogMVCProject/Models/DogViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DogMVCProject.Models
+{
+    public class DogViewModel
+    {
+        public string PetName { get; set; }
+        public string Name { get; set; }
+        public int AgeOfDog { get; set; }
+        public double MassOfDog { get; set; }
+    }
+}
